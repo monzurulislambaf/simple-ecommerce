9a96819745db0ee7ffea40ac7b4dc35ba4377330
monzurulislambaf <monzurulislambaf@gmail.com>
2024-08-19 11:37:24 +0600

    add static product page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row gap-4">
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="https://picsum.photos/seed/picsum/200/300" width="100" height="200"
+                    alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Product title</h5>
+                    <p class="card-text">$20.5</p>
+                    <p class="card-text">Product description</p>
+                    <a href="#" class="btn btn-primary">Add To Cart</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="https://picsum.photos/seed/picsum/200/300" width="100" height="200"
+                    alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Product title</h5>
+                    <p class="card-text">$20.5</p>
+                    <p class="card-text">Product description</p>
+                    <a href="#" class="btn btn-primary">Add To Cart</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="https://picsum.photos/seed/picsum/200/300" width="100" height="200"
+                    alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Product title</h5>
+                    <p class="card-text">$20.5</p>
+                    <p class="card-text">Product description</p>
+                    <a href="#" class="btn btn-primary">Add To Cart</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="https://picsum.photos/seed/picsum/200/300" width="100" height="200"
+                    alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Product title</h5>
+                    <p class="card-text">$20.5</p>
+                    <p class="card-text">Product description</p>
+                    <a href="#" class="btn btn-primary">Add To Cart</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="https://picsum.photos/seed/picsum/200/300" width="100" height="200"
+                    alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Product title</h5>
+                    <p class="card-text">$20.5</p>
+                    <p class="card-text">Product description</p>
+                    <a href="#" class="btn btn-primary">Add To Cart</a>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="https://picsum.photos/seed/picsum/200/300" width="100" height="200"
+                    alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Product title</h5>
+                    <p class="card-text">$20.5</p>
+                    <p class="card-text">Product description</p>
+                    <a href="#" class="btn btn-primary">Add To Cart</a>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
